7a5bffd9d02239dd3ffe58ee23c3d7afc48a94fe
Colm Carew <colm.carew@errigal.com>
2016-08-11 19:21:30 +0100

    User tests

--- test/unit/com/colm/api/UserSpec.groovy
+++ test/unit/com/colm/api/UserSpec.groovy
@@ -1,6 +1,7 @@
 package com.colm.api
 
 import grails.test.mixin.TestFor
+import grails.validation.ValidationException
 import spock.lang.Specification
 
 /**
@@ -9,12 +10,54 @@ import spock.lang.Specification
 @TestFor(User)
 class UserSpec extends Specification {
 
-    def setup() {
-    }
+  UserName userName
+  UserLocation userLocation
+  UserPicture userPicture
+  User perfectUser
 
-    def cleanup() {
-    }
+  def setup() {
+    def mockUserNameAsset = mockFor(UserName)
+    userName = (UserName) mockUserNameAsset.createMock()
 
-    void "test something"() {
-    }
+    def mockUserLocationAsset = mockFor(UserLocation)
+    userLocation = (UserLocation) mockUserLocationAsset.createMock()
+
+    def mockUserPictureAsset = mockFor(UserPicture)
+    userPicture = (UserPicture) mockUserPictureAsset.createMock()
+
+    perfectUser = new User(name: userName, picture: userPicture, location: userLocation, gender: 'male',
+        email: 'colmcarew2@gmail.com', phone: '0833762454', dob: 1243423432, registered: 435345344, password: 'wafflrockones',
+        salt: 'lypI10wj', md5: 'bbdd6140e188e3bf68ae7ae67345df65', sha256: 'ec0705aec7393e2269d4593f248e649400d4879b2209f11bb2e012628115a4eb',
+        sha1: '4572d25c99aa65bbf0368168f65d9770b7cacfe6', PPS: '3423432', username: 'ccarew')
+  }
+
+  def cleanup() {
+  }
+
+  def "A user will save if all information is provided"() {
+    given:
+    perfectUser.validate()
+    when:
+    perfectUser.save(failOnError: true)
+    then:
+    noExceptionThrown()
+  }
+
+  def "User will not save if it does not have all the information"() {
+    given:
+    User u = new User(name: userName, picture: userPicture)
+    when:
+    u.save(failOnError: true)
+    then:
+    ValidationException ex = thrown()
+  }
+
+  def "A user must be male or female"() {
+    given:
+    perfectUser.gender = 'something non existant'
+    when:
+    perfectUser.save(failOnError: true)
+    then:
+    ValidationException ex = thrown()
+  }
 }
